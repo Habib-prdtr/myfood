2ab3c1628dadf4976edc64efb03bdd9f1f09753f
Maulana-Dra <maulana.dra06@gmail.com>
2025-05-20 08:40:13 +0700

    Make Backend Dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Livewire\Livewire;
+use App\Livewire\Pages\HomePage;
+
+// Livewire::route('/', HomePage::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
+
